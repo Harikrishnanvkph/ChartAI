b7f5ab5cdc4a5530371a88d6170a6368a8f4dfeb
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2025-07-05 15:55:45 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import ChartJS from '../lib/chart-registration.ts'
+import '../lib/chart-registration.ts'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -16,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ChartJSRegistration />
         {children}
       </body>
     </html>
